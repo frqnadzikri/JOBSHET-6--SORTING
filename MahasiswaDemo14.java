8490e1826392c921edf5d439af35a4d50c8f23d1
Furqon Adzikri <furqonadzikri311@gmail.com>
2026-04-01 05:53:03 +0700

    Praktikum 2 -Mengurutkan Data Berdasarkan Ipk

--- MahasiswaDemo14.java
+++ MahasiswaDemo14.java
@@ -14,8 +14,12 @@ public class MahasiswaDemo14 {
         System.out.println("Data mahasiswa sebelum sorting:");
         list.tampil();
 
-        System.out.println("Data mahasiswa setelah sorting berdasarkan IPK (DESC):");
+        System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC):");
         list.bubbleSort();
+        list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC):");
+        list.selectionSort();
         list.tampil();
     }
 }
